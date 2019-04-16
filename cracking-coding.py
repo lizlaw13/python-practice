556601ee9250f5bfa93e9f2668165ff4279c9cef
liz lawrence <elizabeth.law13@gmail.com>
2019-04-16 12:54:35 -0700

    Added more cracking coding interview questions

--- cracking-coding.py
+++ cracking-coding.py
@@ -1,3 +1,5 @@
+# ARRAY AND STRING INTERVIEW QUESTIONS
+
 # Is Unique: Implement an algorithm to determine if a string has all unique characters. 
 # What if you cannot use additional data structures? 
 
@@ -46,20 +48,11 @@ def permutation(word_one, word_two):
         else:
             storage_two[letter] += 1
 
-    if len(storage_one) > len(storage_two):
-        for key, value in storage_two.items():
-            if key not in storage_one and value not in storage_one:
-                return False
-            return True
-
-    else:
-        for key, value in storage_one.items():
-            if key not in storage_two and value not in storage_two:
-                return False
-            return True
+    if storage_one == storage_two:
+        return True
+    return False
 
 # Write a method to replace all spaces in a string with '%20'
-
 def urlify(url):
     split_url = url.split()
     result = ""
@@ -70,3 +63,47 @@ def urlify(url):
             result += word
     
     return result
+
+# Given a string, write a function to check if it is a permutation of a palindrome
+def permutation_palindrome(word):
+    count_no_spaces = 0
+    for char in word:
+        if char != " ":
+            count_no_spaces += 1
+    
+    storage_no_spaces = {}
+    for char in word:
+        if char != " ":
+            if char not in storage_no_spaces.keys():
+                storage_no_spaces[char] = 1
+            else:
+                storage_no_spaces[char] += 1
+    
+    one_freq = 0
+    two_freq = 0
+    total_count = 0
+
+    for key, value in storage_no_spaces.items():
+        if value == 1:
+            one_freq += 1
+            total_count += value
+        elif value == 2:
+            two_freq += value
+            total_count += value
+
+    if count_no_spaces > 4 or count_no_spaces == 3:
+        if one_freq == 1 and two_freq == count_no_spaces - 1 and total_count == count_no_spaces:
+            return True
+        
+        return False
+
+    elif count_no_spaces == 4:
+        if two_freq == 4:
+            return True
+        return False
+
+    elif count_no_spaces == 1:
+        return True
+    
+    elif count_no_spaces == 2:
+        return False
